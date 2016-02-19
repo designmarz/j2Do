b32b82f0b4d5c24e5537d824aa2bb92df8976b71
Nick Marazzo <designmarz@gmail.com>
2016-02-18 20:20:42 -0800

    working on saving data

--- app.js
+++ app.js
@@ -1,131 +1,117 @@
-/*global console:true */
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
 $(document).ready(function() {
 
+var storageEnv = 'dashMe_todos_dev';
+
 	var listItemHelper = '<li class="todo-item" data-due="%dueDate%" data-done="%done%">%data% | <a href="#" class="done-btn">Done</a> | <a href="#" class="del-btn">Delete</a></li>';
-	// var todoList = ('#todo-list');
+	var todoList = ('#todo-list');
+	// var todo_item = new Create_todo_item();
+	// console.log( todo_item );
+	$('#todoEnter').val("SET VALUE");
+	testData = "";
 
+	function Create_todo_item(todo) {
+		this.todo 		= todo;
+		this.completed 	= false;
+		this.createdAt 	= Date();
+		this.dueDate 	= $( '#datepicker' ).datepicker( 'getDate' );
+	}
 
+	function logItems(arg) {
+	for (var i = 0; i < arg.length; i++) {
+		console.log(arg[i].todo);
+		console.log(arg[i].dueDate);
+		console.log(arg[i].completed);
+		console.log(arg[i].createdAt);
+		}
+	}
 
-function Create_todo_item(todo) {
-    this.todo 		= todo;
-    this.completed 	= false;
-    this.createdAt 	= Date();
-    this.dueDate 	= $( '#datepicker' ).datepicker( 'getDate' );
-}
+	function refreshClicks() {
+		$('.done-btn').click(function(e) {
+			e.preventDefault();
+			console.log($(this).parent());
+			console.log( 'Done Button Clicked ' );
+			$(this).parent().toggleClass( 'done-item' );
+		});
 
-// var todo_item = new Create_todo_item();
+		$('.del-btn').click(function(e) {
+			e.preventDefault();
+			console.log( 'Delete Button Clicked ' );
+			$(this).parent().remove();
+		});
+	}
 
-// console.log( todo_item );
-$('#todoEnter').val("SET VALUE");
-
-
+	function addItem(arg) {
+		console.log(" addItem function")
+		var newItem = listItemHelper.replace('%data%', arg.todo)
+			.replace('%dueDate%', arg.dueDate)
+			.replace('%done%', arg.completed);
+		$(todoList).append(newItem);
+		refreshClicks();
+		logItems(arg);
+	}
 
 	$('#add-btn').click(function(e) {
 		e.preventDefault();
 		var newTodoVal = $('#todoEnter').val();
-		if (newTodoVal === '') {
-			return false;
-		}
-
+		if (newTodoVal === '') { return false; }
 		var todo_item = new Create_todo_item(newTodoVal);
-
-		var newItem = listItemHelper.replace('%data%', todo_item.todo)
-							.replace('%dueDate%', todo_item.dueDate)
-							.replace('%done%', todo_item.completed);
-
-							// console.log( newItem );
-		// loadTodo()
-		saveTodo(newItem);
+		addItem(todo_item);
+		// saveTodo(todo_item);
 		$('#todoEnter').val("");
 		refreshClicks();
 	});
-
-
-function refreshClicks() {
-	$('.done-btn').click(function(e) {
-		e.preventDefault();
-		console.log( 'Done Button Clicked ' );
-		$(this).parent().toggleClass( 'done-item' );
-	});
-
-	$('.del-btn').click(function(e) {
-		e.preventDefault();
-		console.log( 'Delete Button Clicked ' );
-		$(this).parent().remove();
-	});
-}
-
 
 	// $('#add-btn').click(function(e) {
 	// 	e.preventDefault();
 	// });
 
 	function loadTodo() {
+		console.log("loadTodo");
+		// var itemArray = [];
 		// TODO: find local storage todo's
-		var oldData = function() {
-			var getItem = localStorage.getItem('dashMe_todos_dev');
-			var itemArray = [];
-			return	itemArray;
-			if (getItem === "undefined" ) {
-				return	itemArray;
-			} else {
-				itemArray.push( JSON.parse(getItem) );
-				return itemArray;
-			}
+		// var todo_itemTest1 = new Create_todo_item('Test todo item 1');
+		// var todo_itemTest2 = new Create_todo_item('Test todo item 2');
+		// var todo_itemTest3 = new Create_todo_item('Test todo item 3');
+		// itemArray.push(todo_itemTest1,todo_itemTest2,todo_itemTest3);
 
-		};
-
-		// $.each(oldData, function(i) {
-			// console.log( oldData[i] );
-		// });
-
-		// $(todoList).append();
-
-		console.log("Old Data: " +  oldData() );
-		return oldData();
+		// logItems(itemArray);
+		var getItems = JSON.parse(localStorage.getItem(storageEnv) );
+		for (var i = 0; i < getItems.length; i++) {
+			addItem(getItems[i]);
 		}
-
-	function saveTodo(newToDo) {
-		var oldData = loadTodo();
-		console.log(oldData.length);
-		console.log(" ---------------------------> " + newToDo);
-		var newData = oldData.push( newToDo );
-
-		console.log(" --------------------------- ");
-		console.log("  ");
-		console.log("save todo: load Old Data: " +  newData);
-		console.log("  ");
-		console.log(" --------------------------- ");
-
-		// localStorage.setItem('dashMe_todos_dev', JSON.stringify(newData) );
-		for (var i = 0; i < newData.length; i++) {
-			console.log(newData[i]);
-		}
-
+		return getItems;
 	}
 
+	function saveTodo(newToDo) {
+		console.log("saveTodo");
+		var localData = loadTodo();
 
-
-
-
+		if (localData.length === 0) {
+			localData = [];
+		}
+		// logItems(localData);
+		localData.push( newToDo );
+		localStorage.setItem(storageEnv, JSON.stringify(localData) );
+		console.log("SaveTodo load: ---------->\n" + localStorage.getItem(storageEnv)  );
+	}
 
 	function pageLoad() {
-		var blnk = $('#todoEnter').val();
-		// loadTodo();
-		// saveTodo(blnk);
+		// var blnk = $('#todoEnter').val();
+		// saveTodo("Deveolpment");
+		testData = loadTodo();
+
 		$( '#datepicker' ).datepicker({
 		  defaultDate: +7,
 		  nextText: 'Later'
 		});
-		// console.log(window.localStorage.dashMe_todos);
+		console.log('Page Load');
+		// console.log(localStorage.getItem(storageEnv)  );
 	}
+
+
 
 pageLoad();
 // localStorage.setItem("todoData", JSON.stringify(data));
-
-
 // localStorage.setItem('dashMe_todos', JSON.stringify(todo_item) );
-
-
-
 });
